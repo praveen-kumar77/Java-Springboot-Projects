b1d0672c1bdc96c9f80682d9510735a4895d9ed1
praveen <pkoffice2003@gmail.com>
2025-01-17 20:10:35 +0530

    deleted

--- quiz-app-microservice/quiz-service/src/main/java/com/springlearn/quiz_service/repo/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springlearn.quiz_service.repo;
-
-import com.springlearn.quiz_service.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepo extends JpaRepository<Quiz, Integer> {
-}
